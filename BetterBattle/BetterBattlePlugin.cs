57162af53b5764ddadaaed03e57140f668b8d880
Eradev <admin@eradev.com>
2023-03-03 05:39:17 -0500

    BetterBattle: Added self cast on right click

---
+++ BetterBattle/BetterBattlePlugin.cs
@@ -0,0 +1,76 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using Burst2Flame;
+using Burst2Flame.Observable;
+using HarmonyLib;
+using JetBrains.Annotations;
+using UnityEngine.UI;
+
+namespace eradev.stolenrealm.BetterBattle
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class BetterBattlePlugin : BaseUnityPlugin
+    {
+        // ReSharper disable once NotAccessedField.Local
+        private static ManualLogSource _log;
+
+        [UsedImplicitly]
+        private void Awake()
+        {
+            _log = Logger;
+
+            new Harmony(PluginInfo.PLUGIN_GUID).PatchAll();
+
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        [HarmonyPatch(typeof(ActionSlot), "Start")]
+        public class ActionSlotStartPatch
+        {
+            [UsedImplicitly]
+            private static void Postfix(ActionSlot __instance)
+            {
+                __instance.GetComponent<Button>()
+                    .AddComponentIfNone<ClickAction>()
+                    .OnRightClick = () => TrySelfCast(__instance);
+            }
+
+            private static void TrySelfCast(ActionSlot actionSlot)
+            {
+                var skillBar = Skillbar.Instance;
+
+                if (!skillBar.AllowCasting || actionSlot.disabled || !actionSlot.skillIcon.enabled || actionSlot.cooldownOverlay.enabled)
+                {
+                    return;
+                }
+
+                var selectedCharacter = HexCellManager.instance.MyPlayer;
+                var actionInfo = actionSlot.ActionAndSkill.ActionInfo;
+
+                var canCastInfo = selectedCharacter.CanCast(new StructList<HexCell>
+                {
+                    null
+                }, actionInfo);
+
+                if (!canCastInfo.CanCast)
+                {
+                    return;
+                }
+
+                var target = (TargetInfo)actionInfo.Targets[0];
+                var rangeText = !string.IsNullOrEmpty(actionInfo.TargetTextOverride)
+                    ? OptionsManager.Localize(actionInfo.TargetTextOverride)
+                    : target.UseSimpleTargetingRange
+                        ? actionInfo.GetSimpleRange(selectedCharacter.Character, target).ToString()
+                        : "Special";
+
+                if (rangeText != "0")
+                {
+                    return;
+                }
+
+                selectedCharacter.ExecuteAction(selectedCharacter.Character.Cell, actionInfo);
+            }
+        }
+    }
+}
---
+++ BetterBattle/ClickAction.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace eradev.stolenrealm.BetterBattle
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class ClickAction: MonoBehaviour, IPointerClickHandler
+    {
+        // ReSharper disable MemberCanBePrivate.Global UnassignedField.Global
+        public UnityAction OnLeftClick;
+        public UnityAction OnMiddleClick;
+        public UnityAction OnRightClick;
+        // ReSharper enable MemberCanBePrivate.Global UnassignedField.Global
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            // ReSharper disable once SwitchStatementHandlesSomeKnownEnumValuesWithDefault
+            switch (eventData.button)
+            {
+                case PointerEventData.InputButton.Left:
+                    OnLeftClick?.Invoke();
+
+                    break;
+
+                case PointerEventData.InputButton.Middle:
+                    OnMiddleClick?.Invoke();
+
+                    break;
+
+                case PointerEventData.InputButton.Right:
+                    OnRightClick?.Invoke();
+
+                    break;
+            }
+        }
+    }
+}
